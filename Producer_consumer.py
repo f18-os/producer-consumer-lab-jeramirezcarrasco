a2c0185ccd0a7278242897b510a79ccdf318e991
jorge <georgeramirez01@gmail.com>
2018-11-23 16:21:52 -0700

    Consumer/producer lab

--- Producer_consumer.py
+++ Producer_consumer.py
@@ -6,149 +6,109 @@ import numpy as np
 import base64
 import time
 
-Empty = threading.Semaphore(1)
-Full = threading.Semaphore(0)
-Qlock = threading.Lock()
 
-def extractFrames(fileName, outputBuffer):
-    # Initialize frame count
-    global Qlock
-
+def extractFrames(fileName, outputBuffer, Qlock):
+    # Keep track of the current frame for printing purposes
     count = 0
-
-    # open video file
+    #Read file
     vidcap = cv2.VideoCapture(fileName)
-
-    # read first image
     success, image = vidcap.read()
 
     print("Reading frame {} {} ".format(count, success))
     while success:
-        # get a jpg encoded frame
+        #Translate video frames into jpg
         success, jpgImage = cv2.imencode('.jpg', image)
-        # encode the frame as base 64 to make debugging easier
-        jpgAsText = base64.b64encode(jpgImage)
-        # add the frame to the buffer
-        Empty.acquire()
+        #grabs lock, acquires empty and releases full semaphores
+        EmptyExtract.acquire()
         Qlock.acquire()
-        outputBuffer.put(jpgAsText)
+        #Puts jpg into buffer
+        outputBuffer.put(jpgImage)
         Qlock.release()
-        Full.release()
+        FullExtract.release()
         success, image = vidcap.read()
-        print('Reading frame {} {}'.format(count, success))
         count += 1
+        print('Reading frame {} {}'.format(count, success))
+    #After the video is finish the producer outputs end to signal end of transmission
     print("Frame extraction complete")
-    Empty.acquire()
+    EmptyExtract.acquire()
     Qlock.acquire()
     outputBuffer.put("end")
     Qlock.release()
-    Full.release()
+    FullExtract.release()
 
-def ConvertToGrayscale(Buffer):
-    global Qlock
-    # initialize frame count
+def ConvertToGrayscale(extractionQueue,DisplayQueue,QlockExtract,QlockDisplay):
+
     count = 0
+    #Grabs lock, acquires full and releases emtpy semaphore in the producer side
+    FullExtract.acquire()
+    QlockExtract.acquire()
+    frameAsText = extractionQueue.get()
+    QlockExtract.release()
+    EmptyExtract.release()
 
-    # load the next file
-    Full.acquire()
-    Qlock.acquire()
-    frameAsText = Buffer.get()
-    Qlock.release()
-    Empty.release()
-    # decode the frame
-    jpgRawImage = base64.b64decode(frameAsText)
-    # convert the raw frame to a numpy array
-    jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
-    img = cv2.imdecode(jpgImage, cv2.IMREAD_UNCHANGED)
     while not frameAsText == "end":
+        # Translates jpg
+        jpgImage = np.asarray(bytearray(frameAsText), dtype=np.uint8)
+        img = cv2.imdecode(jpgImage, cv2.IMREAD_UNCHANGED)
         print("Converting frame {}".format(count))
-
-        # convert the image to grayscale
         grayscaleFrame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        outFileName = "{}/grayscale_{:04d}.jpg".format("ass", count)
-        cv2.imwrite(outFileName, grayscaleFrame)
-        # generate output file name
-        jpgAsText = base64.b64encode(grayscaleFrame)
-
-        Empty.acquire()
-        Qlock.acquire()
-        Buffer.put(jpgAsText)
-        Qlock.release()
-        Full.release()
+        #Puts the Jpg into consumer side
+        EmptyDisplay.acquire()
+        QlockDisplay.acquire()
+        DisplayQueue.put(grayscaleFrame)
+        QlockDisplay.release()
+        FullDisplay.release()
         count += 1
+        FullExtract.acquire()
+        QlockExtract.acquire()
+        frameAsText = extractionQueue.get()
+        QlockExtract.release()
+        EmptyExtract.release()
+        #After the convertion its done it signals the displayer that the transmiton is finish
+    EmptyDisplay.acquire()
+    QlockDisplay.acquire()
+    DisplayQueue.put("end")
+    QlockDisplay.release()
+    FullDisplay.release()
 
-        Full.acquire()
-        Qlock.acquire()
-        frameAsText = Buffer.get()
-        Qlock.release()
-        Empty.release()
-
-        # decode the frame
-        jpgRawImage = base64.b64decode(frameAsText)
-        # convert the raw frame to a numpy array
-        jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
-        img = cv2.imdecode(jpgImage, cv2.IMREAD_UNCHANGED)
-
-
-    Empty.acquire()
-    Qlock.acquire()
-    Buffer.put("end")
-    Qlock.release()
-    Full.release()
-
-
-def displayFrames(inputBuffer):
-    # initialize frame count
-    global Qlock
+def displayFrames(DisplayQueue,QlockDisplay):
     count = 0
-    Full.acquire()
-    Qlock.acquire()
-    frameAsText = inputBuffer.get()
-    Qlock.release()
-    Empty.release()
-    # go through each frame in the buffer until the buffer is empty
+    FullDisplay.acquire()
+    QlockDisplay.acquire()
+    frameAsText = DisplayQueue.get()
+    QlockDisplay.release()
+    EmptyDisplay.release()
     while not frameAsText == "end":
-        # get the next frame
-
-
-        # decode the frame
-        jpgRawImage = base64.b64decode(frameAsText)
-
-        # convert the raw frame to a numpy array
-        jpgImage = np.asarray(bytearray(jpgRawImage), dtype=np.uint8)
-
-        # get a jpg encoded frame
-        img = cv2.imdecode(jpgImage, cv2.IMREAD_UNCHANGED)
-
         print("Displaying frame {}".format(count))
-
-        # display the image in a window called "video" and wait 42ms
-        # before displaying the next frame
-        cv2.imshow("Video", img)
+        cv2.imshow("Video", frameAsText)
         if cv2.waitKey(42) and 0xFF == ord("q"):
             break
-
         count += 1
-        Full.acquire()
-        Qlock.acquire()
-        frameAsText = inputBuffer.get()
-        Qlock.release()
-        Empty.release()
+        FullDisplay.acquire()
+        QlockDisplay.acquire()
+        frameAsText = DisplayQueue.get()
+        QlockDisplay.release()
+        EmptyDisplay.release()
     print("Finished displaying all frames")
-    # cleanup the windows
     cv2.destroyAllWindows()
 
-
+#Here i separated the semaphores into an extract side and a display side to have 2 producer/consumer working at the same time
+#I did the same for locks
+EmptyExtract = threading.Semaphore(1)
+FullExtract = threading.Semaphore(0)
+EmptyDisplay = threading.Semaphore(1)
+FullDisplay = threading.Semaphore(0)
+QlockExtract = threading.Lock()
+QlockDisplay = threading.Lock()
 extractionQueue = queue.Queue()
+DisplayQueue = queue.Queue()
 filename = 'clip.mp4'
-
-Extract = threading.Thread(target=extractFrames, args=(filename, extractionQueue,))
-Convert = threading.Thread(target=ConvertToGrayscale, args=(extractionQueue,))
-Display = threading.Thread(target=displayFrames, args=(extractionQueue,))
+#Call the threads
+Extract = threading.Thread(target=extractFrames, args=(filename, extractionQueue,QlockExtract))
+Convert = threading.Thread(target=ConvertToGrayscale, args=(extractionQueue,DisplayQueue,QlockExtract,QlockDisplay))
+Display = threading.Thread(target=displayFrames, args=(DisplayQueue,QlockDisplay))
 Extract.start()
-time.sleep(1)
 Convert.start()
 Display.start()
 
 
-# print ("main loop\n")
